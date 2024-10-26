462c6d552cf432eaa90319d6431b3a2da7c3d99e
Alex Soliman <alexsoliman2002@gmail.com>
2024-10-26 17:52:23 -0400

    FAIT: ajout de la logique pour la cle

--- app/src/main/java/com/example/labo3_alexsoliman/CleDecalage.java
+++ app/src/main/java/com/example/labo3_alexsoliman/CleDecalage.java
@@ -1,6 +1,11 @@
 package com.example.labo3_alexsoliman;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +15,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CleDecalage extends AppCompatActivity {
 
+    EditText et_cle;
+    Button btn_valider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        btn_valider = findViewById(R.id.btn_valider);
+        et_cle = findViewById(R.id.et_cle);
+        btn_valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            int cle = Integer.parseInt(et_cle.getText().toString());
+            if (cle >= 0 && cle <= 25 ) {
+                Intent intent = new Intent();
+                intent.putExtra("cle", cle);
+                setResult(2, intent);
+            }
+            else {
+                Toast.makeText(getBaseContext(), "Cle Invalide", Toast.LENGTH_SHORT).show();
+            }
+            }
+        });
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cle_decalage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -21,4 +47,7 @@ public class CleDecalage extends AppCompatActivity {
             return insets;
         });
     }
+
+
+
 }
--- app/src/main/java/com/example/labo3_alexsoliman/MainActivity.java
+++ app/src/main/java/com/example/labo3_alexsoliman/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.labo3_alexsoliman;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,9 +24,11 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    int cle;
     private String fichier = "monFichier.txt";
     EditText et_origin, et_recevoir;
     Button btn_crypter, btn_decrypter;
+    Intent intent;
     String data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +94,9 @@ public class MainActivity extends AppCompatActivity {
 
         } else if (item.getItemId() == R.id.mnu_cle){
 
+            intent = new Intent(MainActivity.this, CleDecalage.class);
+            startActivityForResult(intent, 2);
+
         } else if (item.getItemId() == R.id.mnu_quit){
 
         }
@@ -99,6 +106,18 @@ public class MainActivity extends AppCompatActivity {
         return true;
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode ==2)
+        {
+            cle = data.getIntExtra("cle", 0);
+        }
+
+    }
+
 
 
 
